aceb165bdc6493b47fdb9158da7f01052e5e36ac
ikkiko <mushroomkik@gmail.com>
2024-07-27 21:50:32 +0700

    Update main.py
    post BMI

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from pydantic import BaseModel
 
 class multipy(BaseModel):
     a: int                        # variable: type of variable
-    b: int    
+    b: int
+
+class BMI(BaseModel):
+    height: int
+    weight: int
+    Name: str   
 
 app = FastAPI()
 
@@ -23,3 +28,16 @@ def read_score(score: int):
 @app.post("/multipy/")
 def multipy(number: multipy):
     return {"result": number.a * number.b}
+
+@app.post("/BMI/")
+def BMI(calbmi: BMI):
+    BmiValue = calbmi.weight / (calbmi.height^2)
+
+    if(Bmivalue < 18.5):
+        return {"calbmi.Name, your BMI is less than 18.5 >> Underweight"}
+    elif(Bmivalue >= 18.5 or Bmivalue <= 24.9):
+        return {"calbmi.Name, your BMI is 18.5—24.9 >> Healthy Weight"}
+    elif(Bmivalue >= 25.0 or Bmivalue <= 29.9):
+        return {"calbmi.Name, your BMI is 25.0—29.9 >> Overweight"}
+    else:
+        return {"calbmi.Name, your BMI is 30.0 >> Above so Obesity"}
