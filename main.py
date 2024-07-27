e96b97578cc7010fb26ea9cc69de13acba4e4705
BankAkira <zenbong@gmail.com>
2024-07-27 21:33:57 +0700

    add multiply post req

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class multiply(BaseModel):
+    a: int
+    b: int
 
 app = FastAPI()
 
@@ -9,3 +14,7 @@ def root():
 @app.get("/scores/{score}")
 def read_score(score: int):
     return {"score": score}
+
+@app.post("/multiply/")
+def multiply(number: multiply):
+    return {"result": number.a * number.b}
