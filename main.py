e9712df3b52642dbdf4dcf3b3f33f04a10faa9aa
ikkiko <mushroomkik@gmail.com>
2024-07-27 21:28:00 +0700

    Update main.py
    add post multipy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class multipy(BaseModel):
+    a: int                        # variable: type of variable
+    b: int    
 
 app = FastAPI()
 
@@ -14,3 +19,7 @@ def read_score(score: int):
         return {"score pass": score}
     else:
         return {"score fail": score}
+    
+@app.post("/multipy/")
+def multipy(number: multipy):
+    return {"result": number.a * number.b}
