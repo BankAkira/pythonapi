bf49aa4264af6f42a8c0de28837fa5a793c82ac6
ikkiko <mushroomkik@gmail.com>
2024-07-27 21:07:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/score/{score}")
+
+def read_score(score: int):
+    if (score > 70):
+        return {"score pass": score}
+    else:
+        return {"score fail": score}
